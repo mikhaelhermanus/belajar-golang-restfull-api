44c1444f664c3ad056f23b4cd2b5f7315e22ecbc
Mikhael Hermanus <mikhaelhermanus2001@gmail.com>
2024-02-06 20:37:28 +0700

    standarisasi product_response

--- model/web/produtcs/products_response.go
+++ model/web/produtcs/products_response.go
@@ -4,6 +4,6 @@ type ProductsResponse struct {
 	// if isempty message , will return undefined
 	Id           int    `json:"id,omitempty"`
 	Name         string `json:"name,omitempty"`
-	CategoryName string `json:"categoryName,omitempty"`
+	CategoryName string `json:"category_name,omitempty"`
 	Message      string `json:"message,omitempty"`
 }
